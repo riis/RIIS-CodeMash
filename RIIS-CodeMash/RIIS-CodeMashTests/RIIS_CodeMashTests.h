95a1cf6466eb0ac0f0d2c21161751add8daa7f62
Brad Dunlap <bdunlap@riis.com>
2012-01-09 16:17:04 -0500

    New

---
+++ RIIS-CodeMash/RIIS-CodeMashTests/RIIS_CodeMashTests.h
@@ -0,0 +1,13 @@
+//
+//  RIIS_CodeMashTests.h
+//  RIIS-CodeMashTests
+//
+//  Created by Brad Dunlap on 1/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RIIS_CodeMashTests : SenTestCase
+
+@end
---
+++ RIIS-CodeMash/RIIS-CodeMashTests/RIIS_CodeMashTests.m
@@ -0,0 +1,32 @@
+//
+//  RIIS_CodeMashTests.m
+//  RIIS-CodeMashTests
+//
+//  Created by Brad Dunlap on 1/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RIIS_CodeMashTests.h"
+
+@implementation RIIS_CodeMashTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RIIS-CodeMashTests");
+}
+
+@end
